cb2f6dbc5865f58f1c3e71d1621e72f743a9b6dc
Brian Clark <brian.clark@steadyserv.com>
2016-08-16 14:25:22 -0400

    update for newest version, change build process

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,10 +2,11 @@
 set -ex
 
 docker build -t "$IMAGE" .
-
+REVISION=$(docker run --rm --entrypoint=$IMAGE node --version) || exit 1
 if [ $TRAVIS_PULL_REQUEST == "false" ] && [ $TRAVIS_BRANCH == "master" ]; then
   docker tag "$IMAGE" "${REPO}:${REVISION}"
   if [ -n "$TAG" ]; then docker tag "$IMAGE" "${REPO}:${TAG}"; fi
+  if [ -n "$MAJOR" ]; then docker tag "$IMAGE" "${REPO}:${MAJOR}"; fi
   docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
   docker push $REPO
 else
