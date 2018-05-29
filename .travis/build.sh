5b237f363aad296c8db6152a4f667f2f49791dd7
Brian Clark <brian@clark.zone>
2018-05-29 12:56:31 -0400

    refactor build process

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 set -ex
 
-docker build -t "$IMAGE" .
-REVISION=$(docker run --rm --entrypoint=node $IMAGE --version) || exit 1
+image_name=
+revision=
+
+die() {
+   [[ -n "$1" ]] && >&2 echo "Error: $1"
+   exit 1
+}
+
+[[ -n "$REPO" ]] || die "REPO required"
+[[ -n "$VERSION" ]] || die "VERSION required"
+
+if [ -n "$DIST" ]; then
+  image_name="${REPO}:XXXX-${DIST}"
+  docker build --tag mybuild --build-arg "NODE_VERSION=$VERSION" "$DIST"
+else
+  image_name="${REPO}:XXXX"
+  docker build --tag mybuild --build-arg "NODE_VERSION=$VERSION" .
+fi
+
+revision=$(docker run --rm --entrypoint=node mybuild --version) || die "unable to determine revision"
+revision=$(echo $revision | sed -e 's/^v//') || die "failed to parse revision"
+
+docker tag mybuild "${image_name/XXXX/$VERSION}"
+docker tag mybuild "${image_name/XXXX/$revision}"
+
+[[ -n "$MAJOR" ]] && docker tag mybuild "${image_name/XXXX/$MAJOR}"
+[[ -n "$TAG" ]] && docker tag mybuild "${REPO}:${TAG}"
+
 if [ $TRAVIS_PULL_REQUEST == "false" ] && [ $TRAVIS_BRANCH == "master" ]; then
-  REVISION=$(echo $REVISION | sed -e 's/^v//') || exit 1
-  docker tag "$IMAGE" "${REPO}:${REVISION}"
-  if [ -n "$TAG" ]; then docker tag "$IMAGE" "${REPO}:${TAG}"; fi
-  if [ -n "$MAJOR" ]; then docker tag "$IMAGE" "${REPO}:${MAJOR}"; fi
   docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
   docker push $REPO
 else
-  echo "skipping docker push"
+  echo "skipping docker push, listing images..."
+  docker image ls
 fi
